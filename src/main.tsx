eb2f22da07a6c057655868f1e75b510455cd2c7a
Luca Barone <baro.luc@gmail.com>
2026-01-03 22:20:31 +0100

    refactor: migrate styles to module CSS, enhance routing with navigation links

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,32 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router";
 
-import "./main.css";
+import styles from "./main.module.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>home</div>} />
+        <Route
+          path="/"
+          element={
+            <div className={styles.mainLayout}>
+              <nav className={styles.nav}>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/stories">Stories</Link>
+              </nav>
+              <main>
+                <Outlet />
+              </main>
+            </div>
+          }
+        >
+          <Route index element={<div>home</div>} />
+          <Route path="about" element={<div>about</div>} />
+          <Route path="stories" element={<div>stories</div>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
