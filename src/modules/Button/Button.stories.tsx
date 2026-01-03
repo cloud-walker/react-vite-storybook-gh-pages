081f93d648f933d834c2c8c1939e2dc635e60919
Luca Barone <baro.luc@gmail.com>
2026-01-03 22:07:42 +0100

    init

---
+++ src/modules/Button/Button.stories.tsx
@@ -0,0 +1,9 @@
+import { type Meta, type StoryObj } from "@storybook/react-vite";
+
+export default {} satisfies Meta;
+
+export const Playground: StoryObj = {
+  render() {
+    return <button>Click me</button>;
+  },
+};
